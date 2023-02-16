cd94f4a195db0a6cbec30bc8018473cce755380a
Omar Mohamed Mohamed ElSayed Hassan <92756055+OmarAboElsayed@users.noreply.github.com>
2023-02-16 15:34:43 +0200

    Update node_pool.tf

--- terraform/node_pool.tf
+++ terraform/node_pool.tf
@@ -1,17 +1,16 @@
-# resource "google_container_node_pool" "nodepool2000" {
-#   name       = "nodepool2000"
-#   location   = "us-central1"
-#   cluster    = google_container_cluster.my-cluster.name
-#   node_count = 1                                              
+resource "google_container_node_pool" "nodepool" {
+  name       = "nodepool"
+  location   = "us-central1-a"
+  cluster    = google_container_cluster.mycluster.id
+  node_count = 1
 
-#   node_config {                                                                                                                                                                                                                                                                                                                                                                                                         
-#     preemptible  = false                                                
-#     machine_type = "e2-small"
-
-#     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-#     service_account = google_service_account.node-service-account.email
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/cloud-platform"
-#     ]                                                                                                                                                                                 
-#   }
-# }
+  node_config {
+    preemptible  = true
+    machine_type = "e2-micro"
+    # custom service account
+    service_account = google_service_account.node-service-account.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
