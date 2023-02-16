96c62b0bcd526e32639c4c8d3d50f6eb315c42e2
Omar Mohamed Mohamed ElSayed Hassan <92756055+OmarAboElsayed@users.noreply.github.com>
2023-02-16 15:55:38 +0200

    Update service_account.tf

--- terraform/service_account.tf
+++ terraform/service_account.tf
@@ -2,12 +2,12 @@
 #it's recommended to create service account for gke 
 resource "google_service_account" "node-service-account" {
   account_id = "node-service-account"
-  project = "omar-mohamed-el-sayed-project"
+  project = var.project
 }
 
 #grant access to service account usinf iam binding
 resource "google_project_iam_binding" "node-service-account-iam" {
-  project = "omar-mohamed-el-sayed-project"
+  project = var.project
   role    = "roles/storage.admin"
   members = [
     "serviceAccount:${google_service_account.node-service-account.email}"
