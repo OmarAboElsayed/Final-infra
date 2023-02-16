09836ff09573e720b062164c497b973b44dff06c
Omar Mohamed Mohamed ElSayed Hassan <92756055+OmarAboElsayed@users.noreply.github.com>
2023-02-16 16:00:08 +0200

    Update subnets.tf

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -7,11 +7,11 @@ resource "google_compute_subnetwork" "management" {
   network       = google_compute_network.myvpc.id
   private_ip_google_access = true
   secondary_ip_range {
-    range_name    = "k8s-pod-range"
+    range_name    = var.deployment
     ip_cidr_range = "10.0.3.0/24"
   }
   secondary_ip_range {
-    range_name    = "k8s-service-range"
+    range_name    = var.service
     ip_cidr_range = "10.0.4.0/24"
   }
 }
