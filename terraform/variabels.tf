1216a904d7e38737b4e0bf6ac0029f4050bb8c84
Omar Mohamed Mohamed ElSayed Hassan <92756055+OmarAboElsayed@users.noreply.github.com>
2023-02-16 15:45:04 +0200

    Create variabels.tf

---
+++ terraform/variabels.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  type = string  
+}
+variable "zone"{
+type =string
+}
+variable "vpc" {
+    type = string 
+}
+
+variable "cidr1" {
+  type = string
+}
+
+
+variable "project" {
+    type=string
+  
+}
+variable "cluster"{
+type=string
+}
+variable "name-subnet"{
+type=string
+}
