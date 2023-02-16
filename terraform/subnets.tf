dc71fcf1bca0d5a3aff5251b58993ff1de0729f2
Omar Mohamed Mohamed ElSayed Hassan <92756055+OmarAboElsayed@users.noreply.github.com>
2023-02-16 15:54:54 +0200

    Update subnets.tf

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,7 +1,7 @@
 
 
 resource "google_compute_subnetwork" "management" {
-  name          = "management"
+  name          = var.subnet
   ip_cidr_range = var.cidr1
   region        = var.region
   network       = google_compute_network.myvpc.id
