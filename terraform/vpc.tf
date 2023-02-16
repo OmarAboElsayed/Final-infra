3a0f33dbee8af1148f9a68e71de1f3df317e0b20
Omar Mohamed Mohamed ElSayed Hassan <92756055+OmarAboElsayed@users.noreply.github.com>
2023-02-16 15:52:20 +0200

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "myvpc" {
-  project                 = "omar-mohamed-el-sayed-project"
-  name                    = "myvpc"
+  project                 = var.project
+  name                    = var.vpc
   auto_create_subnetworks = false
   mtu                     = 1460
   routing_mode            = "REGIONAL"
